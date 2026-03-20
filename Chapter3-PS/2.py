fcf21d157b62b9d8774b4acd24a4e1da29c20c19
gayathrimh11@gmail.com <gayathrimh11@gmail.com>
2026-03-20 23:01:50 +0530

    Chapter 3 practice set added for strings

---
+++ Chapter3-PS/2.py
@@ -0,0 +1,6 @@
+letter='''
+Dear <|Name|>,
+You are selected!
+<|Date|>
+'''
+print(letter.replace("<|Name|>","Gayathri").replace("<|Date|>","24 Sept"))
---
+++ Chapter3-PS/3.py
@@ -0,0 +1,2 @@
+name="harry is good  boy"
+print(name.find("  "))
